984410d6e9c67966f7bb1dbfd1e6d9fe5fc6c36f
jandrei <jandrei.cm@gmail.com>
2018-09-01 21:09:42 -0300

    ajustes

---
+++ extension2.js
@@ -0,0 +1,13 @@
+
+
+var listen_widget = document.getElementById("listen_widget");
+var div_bottoes = document.createElement("div");
+div_bottoes.style = "display: block; margin-left: auto; margin-right: auto; width: 40%;";
+
+var ifrm = document.createElement("iframe");
+ifrm.setAttribute("src", "https://jandrei.github.io/wordreferenceplus/");
+ifrm.style.width = "100%";
+ifrm.style.height = "100%";
+ifrm.frameBorder="0";
+div_bottoes.appendChild(ifrm);
+listen_widget.appendChild(div_bottoes);
